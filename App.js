c4081e341010eb52f3d7ef550ddc2058cc49125c
Nadlss <58627148+Nadlss@users.noreply.github.com>
2020-04-23 11:29:17 +0100

    Added import information on created components and styling files
    - styles.css
    - CityForm
    - CurrentWeather
    - Forecast

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import CityForm from "./CityForm";
+import CurrentWeather from "./CurrentWeather";
+import Forecast from "./Forecast";
 
 export default function App() {
   return (
     <div className="App">
+      <CityForm />
       <div id="location">
         <span id="city">San Juan</span>, <span id="country">Puerto Rico</span>
       </div>
+      <CurrentWeather />
       <div> imagem 3</div>
     </div>
   );
