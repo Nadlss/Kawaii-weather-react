762bd07b9a5524c532734c09ded3116a7421a357
Nadlss <58627148+Nadlss@users.noreply.github.com>
2020-04-23 11:23:06 +0100

    Created App.js file

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div id="location">
+        <span id="city">San Juan</span>, <span id="country">Puerto Rico</span>
+      </div>
+      <div> imagem 3</div>
+    </div>
+  );
+}
